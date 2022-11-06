5cec0ca1c876658394c5b0a3265581bcb9dee8ab
smilelazybone <goodljtnin@gmail.com>
2022-11-06 18:46:49 +0800

    Initialize Restaurant List

---
+++ restaurant_list/README.md
@@ -0,0 +1,38 @@
+# Restaurant List
+A simple web application for search favorite restaurant
+
+## Features
+- listing restaurants from restaurant.json
+- searching restaurants by name and category
+- deriving the information of restaurant when clicking the card
+
+### Searching
+Type name and category of restaurants in search bar
+
+### Prerequisites
+- Node.js
+
+### Installation and execution
+1. Open terminal and clone this project.
+```
+git clone https://github.com/smilelazybone/ac_exercise.git
+```
+2. Entry the folder of this project in terminal.
+```
+cd restaurant_list
+```
+3. Install npm and nodemon
+```
+npm install command
+```
+```
+nodemon installing command
+```
+4. Execute the app.js file.
+```
+nodemon app.js
+```
+5. This sentence in terminal means successful connection.
+```
+Express is running on http://localhost:3000
+```
---
+++ restaurant_list/app.js
@@ -0,0 +1,44 @@
+//require packages used in the project
+const express = require(`express`)
+const app = express()
+const port = 3000
+
+//require express-handlebars 
+const exphbs = require(`express-handlebars`)
+const restaurantList = require(`./restaurant.json`)
+
+//setting engin
+app.engine(`handlebars`, exphbs({ defaultLayout: `main` }))
+app.set(`view engine`, `handlebars`)
+
+//setting static files
+app.use(express.static(`public`))
+
+//render restaurant list
+app.get(`/`, (req, res) => {
+  res.render(`index`, { restaurants: restaurantList.results })
+})
+
+//show restaurant info
+app.get(`/restaurants/:restaurant_id`, (req, res) => {
+  const restaurant = restaurantList.results.find(
+    restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render(`show`, { restaurant: restaurant })
+})
+
+//search 
+app.get(`/search`, (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(
+    restaurant => {
+      return ((restaurant.name.toLowerCase().includes(keyword.toLowerCase().trim())) ||
+        (restaurant.category.toLowerCase().includes(keyword.toLowerCase().trim())))
+    })
+  res.render(`index`, { restaurants: restaurants, keyword: keyword })
+})
+
+//start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+
+})
